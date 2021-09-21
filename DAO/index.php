de80e21de1d91a97860e573b9e0330de9f8bcc43
mmelero <mmelero@gmail.com>
2021-09-20 21:51:25 -0300

    criação da classe Usario metodo de lista

--- DAO/index.php
+++ DAO/index.php
@@ -6,7 +6,22 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM usuarios");
 //echo json_encode($usuarios);
 
-$root = new Usuario();
-$root->loadById(9);
-echo $root;
+//Retorna o usuario de acordo com id informado.
+//$root = new Usuario();
+//$root->loadById(9);
+//echo $root;
+
+//Retorna uma lista de usuarios
+//echo json_encode(Usuario::getList());
+
+//Retorna uma lista de usuarios com os caracteres informados para pesquisa.
+//$search = Usuario::search("le");
+//echo json_encode($search);
+
+//Outra maneira de efetuar lista de usuarios com os caracteres informados para pesquisa, sem atribuir a uma variavel
+//echo json_encode(Usuario::search("me"));
+
+$login = new Usuario();
+$login->login("mmelero","mar@196");
+echo $login;
 ?>
